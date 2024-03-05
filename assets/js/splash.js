d45274ffb2de8c4ff764929f575ecb3b644faaa3
Eric Lee <eric.hyunil.lee@gmail.com>
2024-03-04 18:48:17 -0800

    worked on functionality of the main page when the website is visited. Created event handler for modal verifying the user's age.

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -1,10 +1,27 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.dropdown-trigger');
-//     var instances = M.Dropdown.init(elems, options);
-// })
+const approveAge = $('#approve')
+const declineAge = $('#decline')
+const backgroundBody = $('body')
 
-$('.dropdown-trigger').dropdown();
+
 
 $(document).ready(function(){
-    $('.carousel').carousel();
-  });
+$('.modal').modal();
+});
+
+
+approveAge.on('click', function(){
+    $('#age-verify-container').empty()
+    $('#searchbar').attr('style', 'visibility: visible')
+})
+
+declineAge.on('click', function(){
+    $('.container').empty()
+    $('.container').addClass('center-align')
+    const memeContainer = $('<div>')
+    memeContainer.addClass('memeContainer')
+    const memeImage = $('<img>')
+    memeImage.attr('src', './assets/images/underagememe.jpg')
+    $('.container').append(memeContainer)
+    memeContainer.append(memeImage)
+    document.body.style.backgroundImage = "url(./assets/images/underage.jpg)"
+})
