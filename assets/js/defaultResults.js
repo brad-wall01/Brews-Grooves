93bf4df5200aac03998d55e48cf291d2edbe2f3d
Eric Lee <eric.hyunil.lee@gmail.com>
2024-03-05 15:39:22 -0800

    Needed to open a new branch before working

--- assets/js/defaultResults.js
+++ assets/js/defaultResults.js
@@ -2,28 +2,39 @@
 let lat;
 let lon;
 
-const googleGeocoding = `https://maps.googleapis.com/maps/api/geocode/json?address=irvine%20ca%20usa&key=AIzaSyD1gsa4zv5uIoI49Jw0HZOn2kVv0feSbHg`
-fetch(googleGeocoding).then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    console.log(data)
-    lat = data.results[0].geometry.location.lat
-    lon = data.results[0].geometry.location.lng
-    console.log(lat, lon)
-})
+async function getGeocodeData() {
+   
+
+    const googleGeocoding = `https://maps.googleapis.com/maps/api/geocode/json?address=irvine%20ca%20usa&key=AIzaSyD1gsa4zv5uIoI49Jw0HZOn2kVv0feSbHg`
+    fetch(googleGeocoding).then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+        lat = data.results[0].geometry.location.lat
+        lon = data.results[0].geometry.location.lng
+        console.log(lat)
+        console.log(lon);
+    })
+
+}
+
+async function getBreweryData(lat,lon) {
+    console.log(lat);
+    const openBreweryUrl = `https://api.openbrewerydb.org/v1/breweries?by_dist=${lat},${lon}&per_page=3`
+    console.log(openBreweryUrl);
+
+    fetch(openBreweryUrl).then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+}
 
 
-const openBreweryUrl = `https://api.openbrewerydb.org/v1/breweries?by_city=${city}&by_type=micro&per_page=1000`
-
-fetch(openBreweryUrl).then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    console.log(data)
-})
-
-
+getGeocodeData()
+getBreweryData()
 // const openBreweryUrl = 'https://api.openbrewerydb.org/v1/breweries?by_city=san_diego&per_page=50'
 
 // fetch(openBreweryUrl).then(function(response) {
